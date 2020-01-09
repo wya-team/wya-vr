b721a490e93326802f663511c8434ca4b62cc37a
颜秉斌 <ybb@weiyian.com>
2020-01-09 13:46:54 +0800

    add util history

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
-export const Router = () => {
+import { install } from './install';
+import { inBrowser } from './util/dom';
+import { assert } from './util/warn';
+import { createMatcher } from './create-matcher';
+import { HTML5History } from './history/html5';
+import { AbstractHistory } from './history/abstract';
 
-};
+
+export default class Router {
+	constructor(options = {}) {
+		let mode = options.mode || 'html5';
+		this.matcher = createMatcher(options.routes || []);
+
+		if (!inBrowser) {
+			mode = 'abstract';
+		}
+		this.mode = mode;
+
+		switch (this.mode) {
+			case 'html5':
+				this.history = new HTML5History();
+				break;
+			case 'abstract':
+				this.history = new AbstractHistory();
+				break;
+			default:
+				if (process.env.NODE_ENV !== 'production') {
+					assert(false, `invalid mode: ${mode}`);
+				}
+		}
+	}
+
+	init() {
+		
+	}
+}
+
+Router.install = install;
+
+if (inBrowser && window.app) {
+	window.app.use(Router);
+}
--- src/install.js
+++ src/install.js
@@ -1,7 +1,26 @@
 import View from './components/view';
 import Link from './components/link';
 
+// eslint-disable-next-line import/no-mutable-exports
+export let _Vue;
+
 export function install(Vue) {
+	if (install.installed) return;
+
+	install.installed = true;
+
+	_Vue = Vue;
+
+	Vue.mixin({
+		beforeCreate() {
+			if (this.$options.router) {
+				this._routerRoot = this;
+				this._router = this.$options.router;
+				this._router.init(this);
+				Vue.util.defineReactive(this, '_route', this.$router.history.current);
+			}
+		}
+	});
 
 	Object.defineProperty(Vue.prototype, '$router', {
 		get() {
